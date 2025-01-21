8410535e85d14d93278568729bfa20e566803891
3shaan <3shaan.dev@proton.me>
2025-01-21 21:38:16 +0600

    Enhance Global Validation Pipe: Update main.ts to configure ValidationPipe with whitelist and transform options for improved data validation and performance.

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      // forbidNonWhitelisted: true,
+      // this is to transform the data to the type of the dto and type of the request, its coz performance issue for large data
+      transform: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
